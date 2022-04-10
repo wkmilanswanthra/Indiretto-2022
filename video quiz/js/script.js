5a683d36d2644602d94237af13fb082518fc1b60
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2022-04-10 12:12:37 +0530

    Update script.js

--- video quiz/js/script.js
+++ video quiz/js/script.js
@@ -1,37 +1,3 @@
- //krish
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
-
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAea2eh18zsrKWjcKuBCoVq8qGns-x-Xs8",
-    authDomain: "indiretto-a2516.firebaseapp.com",
-    databaseURL: "https://indiretto-a2516-default-rtdb.firebaseio.com",
-    projectId: "indiretto-a2516",
-    storageBucket: "indiretto-a2516.appspot.com",
-    messagingSenderId: "950979902544",
-    appId: "1:950979902544:web:652e58ebeea76ddbda6555",
-    measurementId: "G-NPN7DEXW95"
-};
-
- // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-import {
-    getFirestore, doc, getDoc, setDoc, collection, addDoc, updateDoc, deleteDoc, deleteField
-} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
-
-const db = getFirestore();
-localStorage.setItem("group","Nimash");
-var teamName = localStorage.getItem('group');
-
-//krish
-
 
 
 //Start Section
@@ -160,6 +126,8 @@ choice_que.forEach((choices, choiceNo) => {
     })
 });
 
+
+let tot = 0;
 ////what happen when 'Next' Button Will Click
 next_question.addEventListener("click", () => {
     //    if index is less then MCQS.length
@@ -187,6 +155,8 @@ next_question.addEventListener("click", () => {
         quiz.style.display = "none";
         points.innerHTML = `You Got ${correct} Out Of ${MCQS.length}`;
         result.style.display = "block";
+        tot = correct;
+        console.log(tot);
     }
     for (i = 0; i <= 3; i++) {
         choice_que[i].classList.remove("disabled");
@@ -197,25 +167,7 @@ next_question.addEventListener("click", () => {
 quit.addEventListener("click", () => {
     start.style.display = "block";
     result.style.display = "none";
+    sendData();
 });
 
 
-
-//krish
-sendData();
-async function sendData(){
-    var ref = collection(db,"riddle");
-
-    const docRef = await addDoc(
-        ref, {
-            TeamName: teamName,
-            Marks:correct
-        }
-    )
-    .then(()=>{
-        alert("data added successfully");
-    })
-    .catch((error)=>{
-        alert("unsuccessfull, error:"+error);
-    })
-}
